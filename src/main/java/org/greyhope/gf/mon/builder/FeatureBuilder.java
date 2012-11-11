69059582928a3c19938b764f68eababa20520a5d
ebe <standing.ablaze@gmail.com>
2012-11-11 22:03:32 +0200

    Adding documentation to classes and methods
    1. Feature Builder and JDOMReader class documentation

--- src/main/java/org/greyhope/gf/mon/builder/FeatureBuilder.java
+++ src/main/java/org/greyhope/gf/mon/builder/FeatureBuilder.java
@@ -5,14 +5,9 @@ import java.beans.Statement;
 import java.lang.reflect.Field;
 import java.util.List;
 import org.greyhope.gf.mon.framework.Feature;
-import org.greyhope.gf.mon.tree.ConnectionQueue;
 import org.jdom2.Attribute;
 import org.jdom2.Element;
 
-/**
- *
- * @author greyhope
- */
 //long count;
 //String highwatermark;
 //long lastsampletime;
@@ -26,16 +21,48 @@ import org.jdom2.Element;
 //String lowwatermark;
 //String mintime;
 //String maxtime;
+
+    /**
+     * 
+     * @author greyhope
+     * 
+     * <p>
+     * This class is used for building features for the glassfish application server that can be monitored. The static
+     * methods take the Feature bundled component that will be interogated and populated with values. The util methods 
+     * will return a feature attributed to a bundled feature component.       
+     * <p>
+     */
 public class FeatureBuilder {
 
     public final static String KEY = "key";
     public final static String VALUE = "value";
     public final static String SET = "set_";
     
+    
+    /**
+     * <p>
+     * The get fields method will extract the fields from the bundled feature class.
+     * <p>
+     *
+     * @param Class clazz
+     * @return Field[]
+     *
+     */
     public static Field[] getFields(Class clazz){
         return clazz.getDeclaredFields();
     }
     
+    /**
+     * <p>
+     * The get fields method will extract the fields from the feature class and populate a feature class from the Nodelist object
+     * using statement and expression introspective elements.
+     * <p>
+     *
+     * @param name
+     * @param elements
+     * @return result
+     *
+     */
     public static Feature buildFeature(String name, List<Element> elements) throws Exception {
 
         Object result = new Feature(name);
